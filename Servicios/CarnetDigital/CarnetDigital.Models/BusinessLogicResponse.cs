f4e1a419feecdd3d202be8ec20fdd8073495f320
Ali <148362656+BEE8030@users.noreply.github.com>
2024-06-05 21:12:11 -0600

    Integración de clases, referencias y paquetes NuGet

---
+++ Servicios/CarnetDigital/CarnetDigital.Models/BusinessLogicResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnetDigital.Models
+{
+    public class BusinessLogicResponse
+    {
+        public int StatusCode { get; set; } = 200;
+        public string Message { get; set; } = null!;
+
+        public BusinessLogicResponse() 
+        { 
+        }
+        public BusinessLogicResponse( int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public BusinessLogicResponse NoContent()
+        {
+            return new BusinessLogicResponse(204, "No Content");
+        }
+
+        public BusinessLogicResponse Success(string message)
+        {
+            return new BusinessLogicResponse(200, message);
+        }
+
+        public BusinessLogicResponse Error(int statusCode, string message)
+        {
+            return new BusinessLogicResponse(statusCode, message);
+        }
+    }
+}

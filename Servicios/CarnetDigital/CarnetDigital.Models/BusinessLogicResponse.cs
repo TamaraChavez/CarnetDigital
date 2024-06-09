79d4bd7b13ac5b206be655b83bfd8577ef25fba1
tamaraChavez <tamara33chavez@gmial.com>
2024-06-09 14:11:00 -0600

    listo bussines logic response

--- Servicios/CarnetDigital/CarnetDigital.Models/BusinessLogicResponse.cs
+++ Servicios/CarnetDigital/CarnetDigital.Models/BusinessLogicResponse.cs
@@ -8,6 +8,30 @@ namespace CarnetDigital.Models
 {
     public class BusinessLogicResponse
     {
+        public int StatusCode { get; set; } = 200;
+        public string Message { get; set; } = null!;
 
+        public BusinessLogicResponse()
+        {
+        }
+        public BusinessLogicResponse(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+        public BusinessLogicResponse NoContent()
+        {
+            return new BusinessLogicResponse(204, "No Content");
+        }
+
+        public BusinessLogicResponse Success(string message)
+        {
+            return new BusinessLogicResponse(200, message);
+        }
+
+        public BusinessLogicResponse Error(int statusCode, string message)
+        {
+            return new BusinessLogicResponse(statusCode, message);
+        }
     }
 }

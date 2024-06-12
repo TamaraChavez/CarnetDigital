52ae8a41265afee0c717d2d36b4c911e86dfcc88
Ali <148362656+BEE8030@users.noreply.github.com>
2024-06-11 23:21:16 -0600

    GENERAR EL SCAFFOLD

---
+++ Servicios/CarnetDigital/CarnetDigital.DataAccess/Models/Area.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarnetDigital.DataAccess.Models;
+
+public partial class Area
+{
+    public byte AreaId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Usuario> Email { get; set; } = new List<Usuario>();
+}

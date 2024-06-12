2cadffce4a0db7fc361fc86035ce2e5794f99b38
Ali <148362656+BEE8030@users.noreply.github.com>
2024-06-11 22:52:35 -0600

    CAMBIOS ALI

---
+++ Servicios/CarnetDigital/CarnetDigital.Models/EstadoDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace CarnetDigital.Catalogs
+
+{
+    public class EstadoDAO
+    {
+        [Required( ErrorMessage = "El estado de la persona es requerido")]
+        public int EstadoID { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerido")]
+        public string Descripcion { get; set; } = null!;
+    }
+}

0a6ed9900f0e2df8bfbc54a372296f28e957bbe0
tamaraChavez <tamara33chavez@gmial.com>
2024-06-12 15:37:52 -0600

    Tamara empieza

--- Servicios/CarnetDigital/CarnetDigital.Pictures/FotografiaEndpoints.cs
+++ Servicios/CarnetDigital/CarnetDigital.Pictures/FotografiaEndpoints.cs
@@ -17,10 +17,10 @@ namespace CarnetDigital.Pictures
     // Este método mapea los endpoints relacionados con las fotografias
     public static void MapFotografiaEndpoints(this IEndpointRouteBuilder routes)
     {
-      var group = routes.MapGroup("/usuario").WithTags(nameof(Usuario));
+      var group = routes.MapGroup("/usuario/fotografia").WithTags(nameof(Usuario));
 
       // Actualizar Foto con validación
-      group.MapPatch("/fotografia/{email}", async Task<Results<Ok, NotFound, BadRequest>> (string email, [FromBody] string fotoBase64, CarnetDigitalDbContext db) =>
+      group.MapPatch("/{email}", async Task<Results<Ok, NotFound, BadRequest>> (string email, [FromBody] string fotoBase64, CarnetDigitalDbContext db) =>
       {
         if (string.IsNullOrEmpty(fotoBase64))
         {
@@ -42,33 +42,11 @@ namespace CarnetDigital.Pictures
       .WithOpenApi();
 
 
-      group.MapPatch("/fotografia{email}", async Task<BusinessLogicResponse> (string email, [FromBody] fotoBase64 FotografiaDAO, CarnetDigitalDbContext db) =>
-            {
-                var tipoIdentificacion = await db.TipoIdentificacion.FindAsync(tipoidentificacionid);
-                if (tipoIdentificacion == null)
-                {
-                    return new BusinessLogicResponse(404, "Tipo de identificación no encontrado.");
-                }
-
-                // Validar el objeto TipoIdentificacionDAO
-                var validationResults = new List<ValidationResult>();
-                if (!Validator.TryValidateObject(tipoIdentificacionDAO, new ValidationContext(tipoIdentificacionDAO), validationResults, true))
-                {
-                    return new BusinessLogicResponse(400, string.Join("; ", validationResults.Select(r => r.ErrorMessage)));
-                }
-
-                tipoIdentificacion.Nombre = tipoIdentificacionDAO.Nombre;
-
-                await db.SaveChangesAsync();
-
-                return new BusinessLogicResponse(200, "Tipo de identificación actualizado exitosamente.");
-            })
-              .WithName("PartialUpdateTipoIdentificacion")
-              .WithOpenApi();
+      
 
 
             // Eliminar Foto
-            group.MapDelete("/fotografia/{email}", async Task<Results<Ok, NotFound>> (string email, CarnetDigitalDbContext db) =>
+            group.MapDelete("/{email}", async Task<Results<Ok, NotFound>> (string email, CarnetDigitalDbContext db) =>
       {
         var usuario = await db.Usuario.FirstOrDefaultAsync(u => u.Email == email);
           if (usuario == null)
@@ -85,7 +63,7 @@ namespace CarnetDigital.Pictures
       .WithOpenApi();
 
       // Obtener Foto
-      group.MapGet("/fotografia/{email}", async Task<Results<Ok<string>, NotFound>> (string email, CarnetDigitalDbContext db) =>
+      group.MapGet("/{email}", async Task<Results<Ok<string>, NotFound>> (string email, CarnetDigitalDbContext db) =>
       {
         var usuario = await db.Usuario.FirstOrDefaultAsync(u => u.Email == email);
         if (usuario == null)

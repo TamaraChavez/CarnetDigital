971e304d696420b8d9db890312eae4b6f11f7ecc
Gabriel Milito <gabrielm@ticodevscr.com>
2024-06-13 23:29:20 -0600

    a

--- Servicios/CarnetDigital/CarnetDigital.Models/UsuarioDAO.cs
+++ Servicios/CarnetDigital/CarnetDigital.Models/UsuarioDAO.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-<<<<<<< HEAD
 using System.Text.Json.Serialization;
 
 namespace CarnetDigital.Models
@@ -178,39 +177,3 @@ namespace CarnetDigital.Models
 
         //    return results;
         //}
-
-=======
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarnetDigital.Models
-{
-    public class UsuarioDAO
-    {
-        [Required(AllowEmptyStrings = false,ErrorMessage = "Email de la persona requrido")]
-        public string Email { get; set; } = null!;
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "tipo de ID de la persona requrido")]
-        public byte TipoIdentificacionId { get; set; }
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "ID de la perosna requrido")]
-        public string Identificacion { get; set; } = null!;
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Nombre completo de la persona requrido")]
-        public string NombreCompleto { get; set; } = null!;
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Contrase\u00F1a de la persona requerida")]
-        public string Contrasena { get; set; } = null!;
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Estado de la persona requrido")]
-        public byte Estado { get; set; }
-
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor indique tipo de usarios")]
-        public byte TipoUsuarioId { get; set; }
-
-
-       
-    }
-
-}
->>>>>>> d95f8b2937232dd174c02909fb7b342e713fcf73
